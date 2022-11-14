a7708bf2913071bdf3cefee9911486ebe03dd811
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-14 00:11:09 -0300

    Adicionando Eventos

---
+++ lib/blocs/bloc/tarefas_event.dart
@@ -0,0 +1,40 @@
+part of 'tarefas_bloc.dart';
+
+// Classes responsáveis pelas ações adicionar, atualizar status e deletar tarefa.
+
+abstract class TarefasEvent extends Equatable {
+  const TarefasEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AdicionarTarefa extends TarefasEvent {
+  final Tarefa tarefa;
+  const AdicionarTarefa({
+    required this.tarefa,
+  });
+
+  @override
+  List<Object> get props => [tarefa];
+}
+
+class AtualizarTarefa extends TarefasEvent {
+  final Tarefa tarefa;
+  const AtualizarTarefa({
+    required this.tarefa,
+  });
+
+  @override
+  List<Object> get props => [tarefa];
+}
+
+class ExcluirTarefa extends TarefasEvent {
+  final Tarefa tarefa;
+  const ExcluirTarefa({
+    required this.tarefa,
+  });
+
+  @override
+  List<Object> get props => [tarefa];
+}
