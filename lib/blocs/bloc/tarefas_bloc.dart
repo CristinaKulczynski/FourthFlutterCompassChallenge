7a69e7f3926501b21fc97495d8adc2c1be3d051d
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-14 15:43:39 -0300

    Criando Map no tarefas state

--- lib/blocs/bloc/tarefas_bloc.dart
+++ lib/blocs/bloc/tarefas_bloc.dart
@@ -1,6 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:todolist/blocs/exportacao_do_bloc.dart';
 import 'package:todolist/models/tarefa.dart';
 
 part 'tarefas_event.dart';
@@ -8,7 +7,7 @@ part 'tarefas_state.dart';
 
 // Tarefas bloc serve para controlar os eventos e emitir um novo estado da tarefa
 
-class TarefasBloc extends Bloc<TarefasEvent, TarefasState> {
+class TarefasBloc extends HydratedBloc<TarefasEvent, TarefasState> {
   TarefasBloc() : super(const TarefasState()) {
     // Lógica para fazer as ações
     on<AdicionarTarefa>(_onAdicionarTarefa);
@@ -25,7 +24,39 @@ class TarefasBloc extends Bloc<TarefasEvent, TarefasState> {
     ));
   }
 
-  void _onAtualizarTarefa(AtualizarTarefa event, Emitter<TarefasState> emit) {}
+  void _onAtualizarTarefa(AtualizarTarefa event, Emitter<TarefasState> emit) {
+    final state = this.state;
+    final tarefa = event.tarefa;
+    // variável para atualizar a tarefa no lugar certo
+    final int index = state.listaDeTodasTarefas.indexOf(tarefa);
 
-  void _onExcluirTarefa(ExcluirTarefa event, Emitter<TarefasState> emit) {}
+    // Criando nova lista e removendo tarefa antiga
+
+    List<Tarefa> listaDeTodasTarefas = List.from(state.listaDeTodasTarefas)
+      ..remove(tarefa);
+    //Se o valor da tarefa for falso, adiciona como verdadeiro e vice-versa
+    tarefa.isConcluida == false
+        ? listaDeTodasTarefas.insert(index, tarefa.copyWith(isConcluida: true))
+        : listaDeTodasTarefas.insert(
+            index, tarefa.copyWith(isConcluida: false));
+    //Emitindo nova tarefa
+    emit(TarefasState(listaDeTodasTarefas: listaDeTodasTarefas));
+  }
+
+  void _onExcluirTarefa(ExcluirTarefa event, Emitter<TarefasState> emit) {
+    final state = this.state;
+    emit(TarefasState(
+        listaDeTodasTarefas: List.from(state.listaDeTodasTarefas)
+          ..remove(event.tarefa)));
+  }
+
+  @override
+  TarefasState? fromJson(Map<String, dynamic> json) {
+    return TarefasState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(TarefasState state) {
+    return state.toMap();
+  }
 }
