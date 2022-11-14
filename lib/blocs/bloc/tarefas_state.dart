23999d50c48a0134de9824e1f5c7f65b832d64a9
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-14 15:45:27 -0300

    Atualizando status da tarefa e adicionando exclusão

--- lib/blocs/bloc/tarefas_state.dart
+++ lib/blocs/bloc/tarefas_state.dart
@@ -7,4 +7,20 @@ class TarefasState extends Equatable {
 
   @override
   List<Object> get props => [listaDeTodasTarefas];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'listaDeTodasTarefas': listaDeTodasTarefas.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory TarefasState.fromMap(Map<String, dynamic> map) {
+    return TarefasState(
+      listaDeTodasTarefas: List<Tarefa>.from(
+        (map['listaDeTodasTarefas'] as List<int>).map<Tarefa>(
+          (x) => Tarefa.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
+    );
+  }
 }
