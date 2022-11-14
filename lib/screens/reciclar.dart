643527df185c8de3076b9b440cce83fc17c181df
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-14 20:59:25 -0300

    Criando tela Reciclar
    Basicamente copiei a tela todo list e modifiquei algumas características

---
+++ lib/screens/reciclar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/screens/menu_lateral.dart';
+import 'package:todolist/widgets/lista_de_tarefas.dart';
+
+class Lixeira extends StatelessWidget {
+  const Lixeira({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Padding(
+          padding: EdgeInsets.only(left: 15),
+          child: Text(
+            'Reciclar lixeira',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      drawer: const MenuLateral(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Center(
+            child: Chip(
+              label: Text(
+                // Mostra quantas tarefas temos
+                ' Tasks',
+              ),
+            ),
+          ),
+          ListaDeTarefas(listaDeTarefas: []),
+        ],
+      ),
+    );
+  }
+}
