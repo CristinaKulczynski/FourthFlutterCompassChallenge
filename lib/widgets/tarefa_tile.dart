6738aa2670689ed05bd2b8f60a7f5c24a7a4aee9
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-15 05:39:43 -0300

    PopUpMenu
    Com a opção editar

--- lib/widgets/tarefa_tile.dart
+++ lib/widgets/tarefa_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:todolist/models/tarefa.dart';
 
 import '../blocs/exportacao_do_bloc.dart';
@@ -19,28 +20,61 @@ class TarefaTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Row(
-          children: [
-            const Icon(Icons.star_outline),
-            Text(
-              tarefa.titulo,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                  // Se a tarefa estiver concluida ficara riscada
-                  decoration:
-                      tarefa.isConcluida! ? TextDecoration.lineThrough : null),
-            ),
-          ],
-        ),
-        Checkbox(
-          value: tarefa.isConcluida,
-          onChanged: (value) {
-            context.read<TarefasBloc>().add(AtualizarTarefa(tarefa: tarefa));
-          },
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const Icon(Icons.star_outline),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tarefa.titulo,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 18,
+                          // Se a tarefa estiver concluida ficara riscada
+                          decoration: tarefa.isConcluida!
+                              ? TextDecoration.lineThrough
+                              : null),
+                    ),
+                    //Text(DateFormat().add_yMEd().format(DateTime.now())),
+                    //Text(DateFormat('dd/MM/yyyy').format(DateTime.now())),
+                    Text(DateFormat().add_yMMMd().format(DateTime.now())),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Checkbox(
+                value: tarefa.isConcluida,
+                onChanged: (value) {
+                  context
+                      .read<TarefasBloc>()
+                      .add(AtualizarTarefa(tarefa: tarefa));
+                },
+              ),
+              PopupMenuButton(
+                itemBuilder: ((context) => [
+                      PopupMenuItem(
+                        child: TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(Icons.edit),
+                          label: const Text('Editar'),
+                        ),
+                      ),
+                    ]),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
