e117ab3dae7e742755eacaf8ba9e616120313c2f
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-15 00:38:22 -0300

    Ajustando Tema

--- lib/blocs/bloc_do_botao/botao_bloc.dart
+++ lib/blocs/bloc_do_botao/botao_bloc.dart
@@ -1,10 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'botao_event.dart';
 part 'botao_state.dart';
 
-class BotaoBloc extends Bloc<BotaoEvent, BotaoState> {
+class BotaoBloc extends HydratedBloc<BotaoEvent, BotaoState> {
   BotaoBloc() : super(const BotaoInitial(botaoValor: false)) {
     on<BotaoAtivadoEvent>((event, emit) {
       emit(
@@ -16,5 +17,15 @@ class BotaoBloc extends Bloc<BotaoEvent, BotaoState> {
         const BotaoState(botaoValor: false),
       );
     });
+  }
+
+  @override
+  BotaoState? fromJson(Map<String, dynamic> json) {
+    return BotaoState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(BotaoState state) {
+    return state.toMap();
   }
 }
--- lib/services/tema.dart
+++ lib/services/tema.dart
@@ -8,32 +8,60 @@ enum Tema {
 class Temas {
   static final temasAplicativo = {
     Tema.darkTema: ThemeData(
-      primaryColor: const Color.fromARGB(255, 27, 27, 27),
-      backgroundColor: const Color.fromARGB(255, 27, 27, 27),
+      brightness: Brightness.dark,
+      primaryColor: Colors.black,
+      primarySwatch: Colors.grey,
+      backgroundColor: const Color(
+        0xFF212121,
+      ),
+      dividerColor: Colors.black45,
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: Color.fromARGB(255, 100, 33, 33),
+        backgroundColor: Colors.white,
       ),
       textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
-          foregroundColor: MaterialStateProperty.all(Colors.white),
-        ),
-      ),
-      textTheme: const TextTheme(subtitle1: TextStyle(color: Colors.white)),
-    ),
-    Tema.lightTema: ThemeData(
-      primaryColor: const Color.fromARGB(255, 255, 255, 255),
-      backgroundColor: const Color.fromARGB(255, 206, 206, 206),
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: Color.fromARGB(255, 100, 33, 33),
-      ),
-      textButtonTheme: TextButtonThemeData(
-        style: ButtonStyle(
-          foregroundColor:
-              MaterialStateProperty.all(const Color.fromARGB(255, 2, 2, 2)),
+          foregroundColor: MaterialStateProperty.all(
+            Colors.white,
+          ),
         ),
       ),
       textTheme: const TextTheme(
-          subtitle1: TextStyle(color: Color.fromARGB(255, 7, 7, 7))),
+        subtitle1: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.grey,
+        unselectedItemColor: Colors.white,
+      ),
+    ),
+    Tema.lightTema: ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.black,
+      primarySwatch: Colors.grey,
+      backgroundColor: const Color(
+        0xFF212121,
+      ),
+      dividerColor: Colors.black45,
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Colors.white,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(
+            Colors.white,
+          ),
+        ),
+      ),
+      textTheme: const TextTheme(
+        subtitle1: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.grey,
+        unselectedItemColor: Colors.white,
+      ),
     ),
   };
 }
