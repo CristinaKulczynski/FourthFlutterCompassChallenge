52a0b374611f44df1297998e9586ab6bfeeff22c
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-14 21:42:30 -0300

    Criando lista de tarefas removidas

--- lib/blocs/bloc/tarefas_state.dart
+++ lib/blocs/bloc/tarefas_state.dart
@@ -3,14 +3,20 @@ part of 'tarefas_bloc.dart';
 class TarefasState extends Equatable {
   final List<Tarefa> listaDeTodasTarefas;
 
-  const TarefasState({this.listaDeTodasTarefas = const <Tarefa>[]});
+  final List<Tarefa> tarefasExcluidas;
+
+  const TarefasState({
+    this.listaDeTodasTarefas = const <Tarefa>[],
+    this.tarefasExcluidas = const <Tarefa>[],
+  });
 
   @override
-  List<Object> get props => [listaDeTodasTarefas];
+  List<Object> get props => [listaDeTodasTarefas, tarefasExcluidas];
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'listaDeTodasTarefas': listaDeTodasTarefas.map((x) => x.toMap()).toList(),
+      'tarefasExcluidas': tarefasExcluidas.map((x) => x.toMap()).toList(),
     };
   }
 
@@ -18,6 +24,11 @@ class TarefasState extends Equatable {
     return TarefasState(
       listaDeTodasTarefas: List<Tarefa>.from(
         (map['listaDeTodasTarefas'] as List<int>).map<Tarefa>(
+          (x) => Tarefa.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
+      tarefasExcluidas: List<Tarefa>.from(
+        (map['tarefasExcluidas'] as List<int>).map<Tarefa>(
           (x) => Tarefa.fromMap(x as Map<String, dynamic>),
         ),
       ),
