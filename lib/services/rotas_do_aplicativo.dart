979b22915cea200de86bdcaf860562ce6c6e7ad4
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-14 21:39:24 -0300

    Gerando rotas

---
+++ lib/services/rotas_do_aplicativo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/screens/Reciclar.dart';
+import 'package:todolist/screens/tela_principal_todo_list.dart';
+
+class RotasDoAplicativo {
+  Route? onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Reciclar.id:
+        return MaterialPageRoute(builder: (_) => const Reciclar());
+      case TelaTODOList.id:
+        return MaterialPageRoute(builder: (_) => const TelaTODOList());
+      default:
+        return null;
+    }
+  }
+}
