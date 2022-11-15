259bfed707f38d775bd6c409b53f64e3c8acfac7
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-15 03:03:30 -0300

    Editando rotas

--- lib/services/rotas_do_aplicativo.dart
+++ lib/services/rotas_do_aplicativo.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/screens/Reciclar.dart';
-import 'package:todolist/screens/principal_todo_list.dart';
-//import 'package:todolist/screens/tela_principal_todo_list.dart';
+import 'package:todolist/screens/tela_todo_list.dart';
 
 class RotasDoAplicativo {
   Route? onGenerateRoute(RouteSettings routeSettings) {
@@ -9,7 +8,7 @@ class RotasDoAplicativo {
       case Reciclar.id:
         return MaterialPageRoute(builder: (_) => const Reciclar());
       case TelaTODOList.id:
-        return MaterialPageRoute(builder: (_) => const TelaTODOList());
+        return MaterialPageRoute(builder: (_) => TelaTODOList());
       default:
         return null;
     }
