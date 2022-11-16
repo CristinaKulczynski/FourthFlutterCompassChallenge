c4c744ed5bdfb288dac1138af4e96fb83ce3f685
CrisKulczynsku <cristinasdk21@gmail.com>
2022-11-15 21:34:39 -0300

    Ajustando main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:todolist/blocs/exportacao_do_bloc.dart';
 import 'package:todolist/screens/tela_todo_list.dart';
-import 'package:todolist/screens/pendencias.dart';
 import 'package:todolist/services/rotas_do_aplicativo.dart';
 import 'package:todolist/services/tema.dart';
 
@@ -42,12 +41,10 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             title: 'TODO List',
             debugShowCheckedModeBanner: false,
-            // Muda as cores do tema de acordo com o estado do botao switch
             theme: state.botaoValor
                 ? Temas.temasAplicativo[Tema.lightTema]
                 : Temas.temasAplicativo[Tema.darkTema],
-            /*Tirar debug da tela*/
-            home: TelaTODOList(),
+            home: const TelaTODOList(),
             onGenerateRoute: rotasDoAplicativo.onGenerateRoute,
           );
         },
